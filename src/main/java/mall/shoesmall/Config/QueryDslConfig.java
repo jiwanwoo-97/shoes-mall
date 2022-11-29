b76b6e4238989833e8da60742590655b854fe953
Wanwoo Ji <jwo9767@naver.com>
2022-11-29 15:03:19 +0900

    Chore : QueryDslConfig Configuration 어노테이션 추가
    message : QueryDslConfig Configuration 어노테이션 추가

--- src/main/java/mall/shoesmall/Config/QueryDslConfig.java
+++ src/main/java/mall/shoesmall/Config/QueryDslConfig.java
@@ -2,10 +2,12 @@ package mall.shoesmall.Config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@Configuration
 public class QueryDslConfig {
 
 
