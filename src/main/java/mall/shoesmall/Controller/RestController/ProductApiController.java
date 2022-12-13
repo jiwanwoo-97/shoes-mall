c7da37834a975b7b0e3661fd640af197256ad676
Wanwoo Ji <jwo9767@naver.com>
2022-12-13 20:28:06 +0900

    fix : ProductApiController 주석 정리 message: ProductApiController 주석 정리

--- src/main/java/mall/shoesmall/Controller/RestController/ProductApiController.java
+++ src/main/java/mall/shoesmall/Controller/RestController/ProductApiController.java
@@ -20,7 +20,7 @@ public class ProductApiController {
     private final ProductService productService;
 
 
-    //주소 리스트
+    //상품 필터 조회
     @PostMapping("/api/products/search")
     public ResponseEntity<List<ProductDto.product_search_response>> searchList2(@RequestBody ProductDto.product_search_request request) {
         List<ProductDto.product_search_response> response = productService.searchFilterList(request);
@@ -32,7 +32,7 @@ public class ProductApiController {
 
 
     // 상품 정보 호출
-    @PostMapping("/api/products/{id}")
+    @GetMapping("/api/products/{id}")
     public ResponseEntity<ProductDto.response> product_info(@PathVariable("id") Long id, Model model) {
         ProductDto.response response = productService.find_product_info(id);
         model.addAttribute("product", response);
@@ -42,7 +42,7 @@ public class ProductApiController {
     }
 
     // 상품 사이즈별 정보 호출
-    @PostMapping("/api/products/size_info/{id}/{size}")
+    @GetMapping("/api/products/size-info/{id}/{size}")
     public ResponseEntity<ProductDto.product_size_info_all_list_response> product_size_info(@PathVariable("id") Long id,
                                                                                             @PathVariable("size") String size) {
         ProductDto.product_size_info_all_list_response response = productService.find_product_size_info(id, size);
@@ -55,7 +55,7 @@ public class ProductApiController {
 
 
     // 상품 판매 정보 호출
-    @PostMapping("/api/products/sell_info/{id}/{size}")
+    @GetMapping("/api/products/sell-info/{id}/{size}")
     public ResponseEntity<ProductDto.response> product_sell_info(@PathVariable("id") Long id,
                                                                       @PathVariable("size") String size, Model model) {
         ProductDto.response response = productService.find_product_buy_info(id,size);
@@ -66,7 +66,7 @@ public class ProductApiController {
     }
 
     // 상품 판매 및 주소 계좌 정보 호출
-    @PostMapping("/api/products/sell_final_info/{id}/{size}/{price}/{date}")
+    @GetMapping("/api/products/sell-final-info/{id}/{size}/{price}/{date}")
     public ResponseEntity<ProductDto.product_sell_final_response> product_sell_final_info(@PathVariable("id") Long productId,
                                                                                           @PathVariable("size") int size,
                                                                                           @PathVariable("price") Long price,
@@ -78,7 +78,7 @@ public class ProductApiController {
     }
 
     // 상품 판매 정보 호출
-    @PostMapping("/api/products/buy_info/{id}/{size}")
+    @GetMapping("/api/products/buy-info/{id}/{size}")
     public ResponseEntity<ProductDto.response> product_buy_info(@PathVariable("id") Long id,
                                                                  @PathVariable("size") String size, Model model) {
         ProductDto.response response = productService.find_product_buy_info(id,size);
@@ -89,7 +89,7 @@ public class ProductApiController {
     }
 
     // 상품 구매 및 주소 카드 정보 호출
-    @PostMapping("/api/products/buy_final_info/{id}/{size}/{price}/{date}")
+    @GetMapping("/api/products/buy-final-info/{id}/{size}/{price}/{date}")
     public ResponseEntity<ProductDto.product_buy_final_response> product_buy_final_info(@PathVariable("id") Long productId,
                                                                                           @PathVariable("size") int size,
                                                                                           @PathVariable("price") Long price,
